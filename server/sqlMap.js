b5e8a5066b891afb4314d3e6fc46d84e425f56e3
Mercy1998 <1143776778@qq.com>
2023-11-08 17:37:49 +0800

    删除

--- server/sqlMap.js
+++
@@ -1,9 +0,0 @@
-var sqlMap = {
-    user:{
-        add:'insert into admins(name,pwd) values (?,?)',
-        save:'insert into score(org,cusNum,type,amount,staff,buyTime,score,inTime) values(?,?,?,?,?,?,?,?)',
-        check:'select * from admins where name=? and pwd=?'
-    }
-}
-
-module.exports = sqlMap;
